ec065307a08c96834ce73eab8948885e52e9ff13
LEEYUSEUNG <59510159+LEEYUSEUNG@users.noreply.github.com>
2021-11-15 22:13:45 +0900

    show video list with title

---
+++ src/app.jsx
@@ -0,0 +1,22 @@
+import { useEffect, useState } from 'react';
+import './app.css';
+import Video_list from './components/video_list/video_list';
+
+function App() {
+  const [videos, setVideos] = useState([]);
+
+  useEffect(()=>{
+    const requestOptions = {
+      method: 'GET',
+      redirect: 'follow'
+    };
+    
+    fetch("https://youtube.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=25&key=AIzaSyC2uC2yaOr78-iap5hggEmpiEcLR6SVrek", requestOptions)
+      .then(response => response.json())
+      .then(result => setVideos(result.items))
+      .catch(error => console.log('error', error));
+  }, []);
+  return <Video_list videos={videos}/>;
+}
+
+export default App;
